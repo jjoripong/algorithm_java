85ad80b7c467b9692f0fb35c060dc06c29b8e3dc
godjoy <tlschgml556@gmail.com>
2021-03-30 23:47:09 +0900

    [chapter02-joy] 2~10 완료

---
+++ Doit/Joy/chapter02/Q2_9.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Q2_9 {
+
+	// 2월 평년 28, 윤년 29
+	static int[][] mdays = {
+			{ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 },
+			{ 31, 29, 31,30, 31, 30, 31, 31, 30, 31, 30, 31 }
+	};
+
+	// 윤년: 4, 100으로 나누어 떨어지거나, 400으로 나누어 떨어지지 않는 수
+	static int isLeap(int year) {
+		return (year % 4 == 0 && year % 100 == 0 || year % 400 != 0) ? 1 : 0;  
+	}
+	
+	// 그 해 남은 일 수 구하기 365 + isLeap(윤년은 366일임) - (총 합)
+	static int leftDayOfYear(int y, int m, int d) {
+		while(--m > 0) {
+			d += mdays[isLeap(y)][m-1];
+		}
+		return 365 + isLeap(y) - d;
+	}
+	
+	public static void main(String args[]) {
+		Scanner stdIn = new Scanner(System.in);
+		int retry;
+		
+		do {
+			System.out.println("년: "); int year = stdIn.nextInt();
+			System.out.println("월: "); int month = stdIn.nextInt();
+			System.out.println("일: "); int day = stdIn.nextInt();
+
+			System.out.printf("%d일 남았습니다.\n", leftDayOfYear(year, month, day));
+			
+			System.out.println("계속 하시겠습니까? (1 예 / 2 아니오)");
+			retry = stdIn.nextInt();
+		} while (retry == 1);
+	}
+}
