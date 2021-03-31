84530aadccb68449327c4dbcc389015d392d6325
Ria Ha <jiyoung080912@gmail.com>
2021-03-31 12:46:10 +0900

    Q11 2021.03.31

--- Doit/Ria/chapter02/practice11.java
+++ Doit/Ria/chapter02/practice11.java
@@ -12,12 +12,56 @@ public class practice11 {
         }
 
         YMD after(int n){
-
+        	YMD aTime = new YMD(this.y,this.m,this.d);
+        	int year = aTime.y;
+        	aTime.y = y-(n/365);
+        	int remDate = (n%365);
+        	while(aTime.d - remDate <=0){
+        		aTime.m--;
+        		if(aTime.m == 0) {
+        			aTime.m = 12;
+        			year--;
+        		}
+        		remDate = (aTime.d - remDate)*-1;
+        		aTime.d = mdays[isLeap(year)][aTime.m-1];
+        	}        	
+        	aTime.d -= remDate;
+        	return aTime;
         }
 
-        YMD befor(int n){
-
-        }
+		
+		 YMD before(int n){
+			 YMD bTime = new YMD(this.y,this.m,this.d);
+			 int year = bTime.y;
+			 int day = bTime.d;
+			 bTime.y = y+(n/365);
+			 int remDate = n; //3
+			 
+			 while(day+remDate > mdays[isLeap(year)][bTime.m-1]){
+				//day = 31
+				//if(day+remDate > mdays[isLeap(year)][bTime.m-1]) {
+				 //System.out.println("remDate : "+remDate);
+				if(mdays[isLeap(year)][bTime.m-1]== remDate) {
+					 remDate = 0;
+				}else {
+					remDate -= (mdays[isLeap(year)][bTime.m-1]-day-1 < 0)? 1:mdays[isLeap(year)][bTime.m-1]-day-1;
+				}
+				//System.out.println("remDate : "+remDate);
+					bTime.m++;
+					if(bTime.m == 12) {
+						bTime.m = 1;
+						year++;
+					}
+					day = 1;
+				//}
+			 };
+			 //System.out.println("day : "+day);
+			 //System.out.println("remDate ff: "+remDate);
+			 bTime.d = day+remDate;
+			 //System.out.println("bTime.d : "+bTime.d);
+			 return bTime;
+		 }
+		 
     }
 
     static int[][] mdays = {
@@ -29,21 +73,36 @@ public class practice11 {
         return (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) ? 1 : 0;
     }
 
-    static int dayOfYear(int y, int m, int d) {
-        int day = 0;
-        while (m<=12){
-            day += mdays[isLeap(y)][m-1];
-            m++;
-        }
-        return day-d;
-    }
-
     public static void main(String[] args) {
-
         YMD cTime = new YMD(2021,3,31);
+        
+        YMD aTime = cTime.after(3);
+        YMD aTime1 = cTime.after(31);
+        YMD aTime2 = cTime.after(366);
+        
+        System.out.println("현재날짜 : "+cTime.y+"년 "+cTime.m+"월 "+cTime.d+"일\n");
+        System.out.println("after 3 : "+aTime.y+"년 "+aTime.m+"월 "+aTime.d+"일");
+        System.out.println("after 31 : "+aTime1.y+"년 "+aTime1.m+"월 "+aTime2.d+"일");
+        System.out.println("after 366 : "+aTime2.y+"년 "+aTime2.m+"월 "+aTime2.d+"일");
+        
+        YMD bTime = cTime.before(3);
+        YMD bTime1 = cTime.before(31);
+        YMD bTime2 = cTime.before(365);
+        
+        System.out.println("before 3 : "+bTime.y+"년 "+bTime.m+"월 "+bTime.d+"일");
+        System.out.println("before 31 : "+bTime1.y+"년 "+bTime1.m+"월 "+bTime1.d+"일");
+        System.out.println("before 365 : "+bTime2.y+"년 "+bTime2.m+"월 "+bTime2.d+"일");
     }
 }
 
 /*
 print:
+현재날짜 : 2021년 3월 31일
+
+after 3 : 2021년 3월 28일
+after 31 : 2021년 2월 30일
+after 366 : 2020년 3월 30일
+before 3 : 2021년 4월 3일
+before 31 : 2021년 4월 1일
+before 365 : 2022년 5월 25일
 */
